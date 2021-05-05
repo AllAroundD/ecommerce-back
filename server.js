e600a741ac6375419f5a3bb140af6b2ed5c1f7a2
AllAroundD <AllAroundDWeb@gmail.com>
2021-05-05 17:17:29 -0400

    removed reference to dotenv

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { readdirSync } = require('fs')
-require('dotenv').config()
+// require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 // app
 const app = express()
